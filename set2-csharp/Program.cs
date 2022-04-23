5284da31b4659ec71903d89212dce2bbf88465b2
Rodrigo Sandeski <sandeski47@gmail.com>
2022-04-23 14:27:21 -0300

    Finish

--- set2-csharp/Program.cs
+++ set2-csharp/Program.cs
@@ -10,6 +10,25 @@ namespace set2_csharp
     {
         static void Main(string[] args)
         {
+            HashSet<int> students = new HashSet<int>();
+
+            char letter = 'A'; 
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("How many students for Course " + letter + " : " );
+                int count = int.Parse( Console.ReadLine());
+                for (int j = 0; j < count; j++)
+                {
+                    int aux = int.Parse(Console.ReadLine());
+                    students.Add(aux);
+                }
+
+                letter++;
+                
+            }
+            Console.WriteLine("Total Students : " +  students.Count);
+            
+            Console.ReadKey();
         }
     }
 }
